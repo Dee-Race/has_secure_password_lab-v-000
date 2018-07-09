e339bbff3996886049a1515723d56c9855b2f556
Dee Race <deerace12@gmail.com>
2018-07-09 15:55:28 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params).save 
+    user = User.new(user_params).save
   end
 
-  private 
+  private
 
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end 
-  
+
 end
